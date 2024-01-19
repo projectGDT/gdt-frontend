dd62500c3c6f92d2559c82bb7e8a65ecee1f799d
Wesley F. Young <willamwei@outlook.com>
2024-01-19 12:51:19 +0800

    repair an unnecessary import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,5 @@
 "use client"
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import {
     Alert,
     Box,
