18e1abbae29e75115523c1e870628d13687b1e74
Wesley F. Young <willamwei@outlook.com>
2024-02-06 00:30:06 +0800

    clean code

--- src/app/server/[id]/manage/page.tsx
+++ src/app/server/[id]/manage/page.tsx
@@ -1,41 +1,10 @@
 "use client"
 
-import {
-    Box,
-} from "@mui/material";
-import React, {useEffect, useId, useRef, useState} from "react";
+import React from "react";
 import {useRouter} from "next/navigation";
-import {useSessionStorage} from "usehooks-ts";
 
-import {dict} from "@/i18n/zh-cn"
-
-// @/templates 定义了一些常量和模板
-import Script from "next/script";
-import { Image } from "@mui/icons-material";
-import { space } from "postcss/lib/list";
-
-import mc, { NewPingResult, OldPingResult } from 'minecraft-protocol';
-import { useSearchParams } from "react-router-dom";
-
-// 定义一种错误, 和网络错误区分开, 后面会有用
-class IncorrectCredentialsError extends Error {
-    constructor(props?: string | undefined) {
-        super(props || undefined);
-    }
-}
-
-// 这一块的内容会套在 /src/app/layout.jsx 定义的东西里面
 export default function Page({ params }:{ params: { id: number } }) {
     const router = useRouter()
-    const formRef = useRef()
-
-    // React 中的 State, 一种可以在多次渲染之间暂存的变量, 详见文档
-    const [incorrectCredentialsOpen, setIncorrectCredentialsOpen] = useState(false)
-    const [networkErrorOpen, setNetworkErrorOpen] = useState(false)
-
-    const [id, setId] = useSessionStorage("id", -1)
-    const [isSiteAdmin, setIsSiteAdmin] = useSessionStorage("isSiteAdmin", false)
-    const [jwt, setJWT] = useSessionStorage("jwt", "")
 
     return (
         <p>管理{params.id}号服务器</p>
--- src/app/server/[id]/page.tsx
+++ src/app/server/[id]/page.tsx
@@ -3,32 +3,16 @@
 import {
     Box, Button, Card, CardContent, CardHeader, Checkbox, Divider, FormControlLabel, Grid, Stack, Typography,
 } from "@mui/material";
-import React, {useEffect, useId, useRef, useState} from "react";
+import React from "react";
 import {useRouter} from "next/navigation";
 import {useSessionStorage} from "usehooks-ts";
 
 import {dict} from "@/i18n/zh-cn"
 
-// @/templates 定义了一些常量和模板
-import Script from "next/script";
-import { Image } from "@mui/icons-material";
-import { space } from "postcss/lib/list";
-
-import mc, { NewPingResult, OldPingResult } from 'minecraft-protocol';
-import { useSearchParams } from "react-router-dom";
-
-
 const ApplyingPolicy = {
     CLOSED: "CLOSED",
     ALL_OPEN: "ALL_OPEN",
     BY_FORM: "BY_FORM"
-}
-
-// 定义一种错误, 和网络错误区分开, 后面会有用
-class IncorrectCredentialsError extends Error {
-    constructor(props?: string | undefined) {
-        super(props || undefined);
-    }
 }
 
 const TESTINFO = [
@@ -283,15 +267,6 @@ function VersionInfo(props: {}) {
 // 这一块的内容会套在 /src/app/layout.jsx 定义的东西里面
 export default function Page({ params }:{ params: { id: string } }) {
     const router = useRouter()
-    const formRef = useRef()
-
-    // React 中的 State, 一种可以在多次渲染之间暂存的变量, 详见文档
-    const [incorrectCredentialsOpen, setIncorrectCredentialsOpen] = useState(false)
-    const [networkErrorOpen, setNetworkErrorOpen] = useState(false)
-
-    const [id, setId] = useSessionStorage("id", -1)
-    const [isSiteAdmin, setIsSiteAdmin] = useSessionStorage("isSiteAdmin", false)
-    const [jwt, setJWT] = useSessionStorage("jwt", "")
 
     const [joinedServers, setJoinedServers] = useSessionStorage("joinedServers", [-1]);
 
