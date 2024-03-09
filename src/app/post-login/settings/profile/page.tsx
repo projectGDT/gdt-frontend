2b75c1f7463b637101a701cee54fabd05e7dddae
Wesley F. Young <willamwei@outlook.com>
2024-03-10 07:58:10 +0800

    add a title typography to settings/profile

--- src/app/post-login/settings/profile/page.tsx
+++ src/app/post-login/settings/profile/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import {
+    Box,
     Button,
     Divider,
     IconButton,
@@ -52,85 +53,94 @@ export default function Page() {
         }
     }, [userModifyFlag])
 
-    return <Paper><List>
-        {loading ? <>
-            <Skeleton width={"100%"} height={100}/>
-            <Skeleton width={"100%"} height={100}/>
-            <Skeleton width={"100%"} height={100}/>
-        </> : <>
-            <ListSubheader>{dict.settings.profile.javaMicrosoft.title}</ListSubheader>
-            {javaMsProfile ? (
-                <ListItem disablePadding
-                          key={javaMsProfile.uniqueIdProvider}
-                          secondaryAction={<IconButton onClick={() => confirm({
-                              description: dict.settings.profile.onDelete,
-                          }).then(() => {
-                              setLoading(true) // 因为内容发生改变，所以先“掩人耳目”
-                              fetch(`${backendAddress}/post-login/profile/delete`, POST({
-                                  uniqueIdProvider: -1
-                              })).then(_response => setUserModifyFlag(prev => prev + 1))
-                              // 这会让 useEffect 重新执行一遍
-                          })}>
-                              <DeleteOutlineOutlined/>
-                          </IconButton>}>
-                    <ProfileDisplayButton {...javaMsProfile}/>
-                </ListItem>
-            ) : (
-                <ListItem secondaryAction={<Button href={"profile/java-microsoft"}>
-                    {dict.settings.profile.doBind}
-                </Button>}>
-                    <ListItemText secondary={dict.settings.profile.javaMicrosoft.fallback}/>
-                </ListItem>
-            )}
+    return <Box sx={{display: "flex", flexDirection: "column", gap: 2}}>
+        <Box sx={{textAlign: "center"}}>
+            <Typography variant={"h5"}>{dict.settings.profile.title}</Typography>
+        </Box>
+        <Paper><List>
+            {loading ? <>
+                <Skeleton width={"100%"} height={100}/>
+                <Skeleton width={"100%"} height={100}/>
+                <Skeleton width={"100%"} height={100}/>
+            </> : <>
+                <ListSubheader>{dict.settings.profile.javaMicrosoft.title}</ListSubheader>
+                {javaMsProfile ? (
+                    <ListItem
+                        disablePadding
+                        key={javaMsProfile.uniqueIdProvider}
+                        secondaryAction={<IconButton onClick={() => confirm({
+                            description: dict.settings.profile.onDelete,
+                        }).then(() => {
+                            setLoading(true) // 因为内容发生改变，所以先“掩人耳目”
+                            fetch(`${backendAddress}/post-login/profile/delete`, POST({
+                                uniqueIdProvider: -1
+                            })).then(_response => setUserModifyFlag(prev => prev + 1))
+                            // 这会让 useEffect 重新执行一遍
+                        })}
+                    >
+                        <DeleteOutlineOutlined/>
+                    </IconButton>}>
+                        <ProfileDisplayButton {...javaMsProfile}/>
+                    </ListItem>
+                ) : (
+                    <ListItem secondaryAction={<Button href={"profile/java-microsoft"}>
+                        {dict.settings.profile.doBind}
+                    </Button>}>
+                        <ListItemText secondary={dict.settings.profile.javaMicrosoft.fallback}/>
+                    </ListItem>
+                )}
 
-            <Divider/>
+                <Divider/>
 
-            <ListSubheader>{dict.settings.profile.xbox.title}</ListSubheader>
-            {xboxProfile ? (
-                <ListItem disablePadding
-                          key={xboxProfile.uniqueIdProvider}
-                          secondaryAction={<IconButton onClick={() => confirm({
-                              description: dict.settings.profile.onDelete,
-                          }).then(() => {
-                              setLoading(true)
-                              fetch(`${backendAddress}/post-login/profile/delete`, POST({
-                                  uniqueIdProvider: -3
-                              })).then(_response => setUserModifyFlag(prev => prev + 1))
-                          })}>
-                              <DeleteOutlineOutlined/>
-                          </IconButton>}>
-                    <ProfileDisplayButton {...xboxProfile}/>
-                </ListItem>
-            ) : (
-                <ListItem disablePadding secondaryAction={<Button
-                    href={xboxOauthUri}
-                >{dict.settings.profile.doBind}</Button>}>
-                    <ListItemButton>
-                        <ListItemText secondary={dict.settings.profile.xbox.fallback}/>
+                <ListSubheader>{dict.settings.profile.xbox.title}</ListSubheader>
+                {xboxProfile ? (
+                    <ListItem
+                        disablePadding
+                        key={xboxProfile.uniqueIdProvider}
+                        secondaryAction={<IconButton onClick={() => confirm({
+                            description: dict.settings.profile.onDelete,
+                        }).then(() => {
+                            setLoading(true)
+                            fetch(`${backendAddress}/post-login/profile/delete`, POST({
+                                uniqueIdProvider: -3
+                            })).then(_response => setUserModifyFlag(prev => prev + 1))
+                        })}
+                    >
+                        <DeleteOutlineOutlined/>
+                    </IconButton>}>
+                        <ProfileDisplayButton {...xboxProfile}/>
+                    </ListItem>
+                ) : (
+                    <ListItem disablePadding secondaryAction={<Button
+                        href={xboxOauthUri}
+                    >{dict.settings.profile.doBind}</Button>}>
+                        <ListItemButton>
+                            <ListItemText secondary={dict.settings.profile.xbox.fallback}/>
+                        </ListItemButton>
+                    </ListItem>
+                )}
+
+                <Divider/>
+
+                <ListSubheader>{dict.settings.profile.offline.title}</ListSubheader>
+                <ListItem><ListItemText secondary={dict.settings.profile.offline.fallback}/></ListItem>
+                {offlineProfiles.map(profile => <ListItem disablePadding key={profile.uniqueIdProvider}>
+                    <ListItemButton href={`/server/${profile.uniqueIdProvider}`}>
+                        <ListItemText primary={`${profile.uniqueId}`}
+                                      secondary={dict.settings.profile.offline.secondary(profile.uniqueIdProvider)}/>
                     </ListItemButton>
+                </ListItem>)}
+
+                <Divider/>
+
+                <ListItem>
+                    <ListItemText>
+                        <Typography variant={"caption"}>
+                            Minecraft Head API provided by <Link href="https://minotar.net/">minotar.net</Link>
+                        </Typography>
+                    </ListItemText>
                 </ListItem>
-            )}
-
-            <Divider/>
-
-            <ListSubheader>{dict.settings.profile.offline.title}</ListSubheader>
-            <ListItem><ListItemText secondary={dict.settings.profile.offline.fallback}/></ListItem>
-            {offlineProfiles.map(profile => <ListItem disablePadding key={profile.uniqueIdProvider}>
-                <ListItemButton href={`/server/${profile.uniqueIdProvider}`}>
-                    <ListItemText primary={`${profile.uniqueId}`}
-                                  secondary={dict.settings.profile.offline.secondary(profile.uniqueIdProvider)}/>
-                </ListItemButton>
-            </ListItem>)}
-
-            <Divider/>
-
-            <ListItem>
-                <ListItemText>
-                    <Typography variant={"caption"}>
-                        Minecraft Head API provided by <Link href="https://minotar.net/">minotar.net</Link>
-                    </Typography>
-                </ListItemText>
-            </ListItem>
-        </>}
-    </List></Paper>
+            </>}
+        </List></Paper>
+    </Box>
 }
